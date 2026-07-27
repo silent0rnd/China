8f1c687d9457c2f3a55cf70477c7ef9e7303a12b
Maxim <silent0rnd@gmail.com>
2026-07-27 12:31:19 +0300

    2.1

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['dist/**', 'node_modules/**', '.agents/**'],
   },
   js.configs.recommended,
   {
